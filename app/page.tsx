324a02e53c7c2790ff40758e83d6596d662acba2
MoAamir786 <134404066+MoAamir786@users.noreply.github.com>
2024-06-16 21:02:01 +0200

    New Exp Patch

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import { navItems } from "@/data/index";
 import PricingPage from "@/components/PricingPage";
+import Experience from "@/components/Experience";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems}/>
       <Hero /> 
+      <Experience />
       <RecentProjects />
       <Clients />
       <PricingPage />
